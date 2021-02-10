9072d2ef4cabd63b19a7ee91c4bcaebb18b5ab03
Sviut <jeleznov88@gmail.com>
2021-02-10 07:06:08 +0300

    add simple template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <QuestionCard/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuestionCard from '@/components/QuestionCard'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    QuestionCard,
+  },
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  width: 100vw;
 }
 </style>
---
+++ src/components/QuestionCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrapper">
+    <div class="progress-wrapper">
+      <div class="progress-bar"></div>
+    </div>
+
+    <div class="card-wrapper">
+      <div class="card-title">Что для вас больше всего важно в мастере?</div>
+      <div class="card-answers">
+        <div class="card-answer">Вариант 1</div>
+        <div class="card-answer">Вариант 2</div>
+        <div class="card-answer">Вариант 3</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuestionCard',
+}
+</script>
+
+<style scoped>
+.wrapper {
+  padding: 20px;
+}
+
+.card-wrapper {
+  width: 100%;
+  padding: 15px;
+  border: 1px solid red;
+}
+
+.progress-wrapper {
+  width: 100%;
+}
+
+.progress-bar {
+  /*background-color: #fff;*/
+  background-color: red;
+  height: 4px;
+  width: 100%;
+  animation: grow 5s linear;
+  transform-origin: left;
+}
+
+@keyframes grow {
+  0% {
+    transform: scaleX(0);
+  }
+}
+</style>
