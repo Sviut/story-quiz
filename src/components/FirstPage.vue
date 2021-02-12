6443581c17bfb6cc1d1b3bf7e069418bded96381
Sviut <jeleznov88@gmail.com>
2021-02-12 06:47:48 +0300

    add awesome icons

--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -10,7 +10,7 @@
       <div class="text-subtitle">
         Опрос займет всего 30 секунд!
       </div>
-      <button :style="{background: getGradient}" class="btn">Пройти опрос</button>
+      <button :style="{background: getGradient}" class="btn"><i class="far fa-check-circle"></i> Пройти опрос</button>
     </div>
   </div>
 </template>
