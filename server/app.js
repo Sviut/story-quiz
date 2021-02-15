817e33334ccaabb9862702fedd651bbeda4d8876
Sviut <jeleznov88@gmail.com>
2021-02-15 19:32:16 +0300

    process.env.PORT update

--- server/app.js
+++ server/app.js
@@ -29,5 +29,5 @@ app.post('/new', function (req, res) {
 	})
 })
 
-app.listen(3000,
-	() => console.log('Server is running...' + 3000))
+app.listen(process.env.PORT || 3000,
+	() => console.log('Server is running...'))
