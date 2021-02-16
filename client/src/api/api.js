8027d3f6a3edb5558c19da8ba7a34583feded64f
Sviut <jeleznov88@gmail.com>
2021-02-16 05:31:42 +0300

    add prod url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export async function sendLead (lead) {
 	try {
-		await axios.post('http://localhost:3000/new', lead)
+		await axios.post('https://story-quiz.herokuapp.com/new', lead)
 	} catch (e) {
 		console.log(e)
 	}
