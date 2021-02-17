333f92ca175eb8d5540ada8e9bfe1051cef84e1b
Sviut <jeleznov88@gmail.com>
2021-02-17 11:21:01 +0300

    add yandex metrika

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueSocialSharing from 'vue-social-sharing'
+import VueYandexMetrika from 'vue-y-metrika'
 
 Vue.config.productionTip = false
 
 Vue.use(VueSocialSharing)
+
+Vue.use(VueYandexMetrika, {
+	id: 72568744,
+	env: process.env.NODE_ENV,
+})
 
 new Vue({
 	render: h => h(App),
