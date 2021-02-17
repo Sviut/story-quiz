2416ff2c75bac537e5a9322f1d528c7b534afc06
Sviut <jeleznov88@gmail.com>
2021-02-17 11:59:37 +0300

    update yandex metrika

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueSocialSharing from 'vue-social-sharing'
-import VueYandexMetrika from 'vue-y-metrika'
 
 Vue.config.productionTip = false
 
 Vue.use(VueSocialSharing)
 
-Vue.use(VueYandexMetrika, {
-	id: 72568744,
-	env: process.env.NODE_ENV,
-})
+function initMetrika () {
+	(function (m, e, t, r, i, k, a) {
+		m[i] = m[i] || function () {(m[i].a = m[i].a || []).push(arguments)}
+		m[i].l = 1 * new Date()
+		k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+	})
+		// eslint-disable-next-line no-unexpected-multiline
+		(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym')
+	window.ym(72568744, 'init', {
+		defer: true,
+		accurateTrackBounce: true,
+		webvisor: true,
+	})
+}
+
+if (process.env.NODE_ENV === 'production') {
+	initMetrika()
+}
 
 new Vue({
 	render: h => h(App),
