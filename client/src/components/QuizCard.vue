89343396892a8fd8e68672de6ffd6591e1e1bb4e
Sviut <jeleznov88@gmail.com>
2021-02-24 08:31:39 +0300

    add section for page

--- client/src/components/QuizCard.vue
+++ client/src/components/QuizCard.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="wrapper"></div>
+  <div class="wrapper">
+    <div class="top-section">
+      <div class="avatar-img">
+        <img src="../assets/logo.png" alt="">
+      </div>
+      <div class="title">
+        Ответьте на несколько простых вопросов и мы запишем вас на процедуру за 190 руб.
+      </div>
+      <div class="sub-title">Опрос займет всего 30 секунд!</div>
+    </div>
+
+    <div class="bottom-sectoion">
+      <div>BUtton section</div>
+      <div>BUtton section</div>
+      <div>BUtton section</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,5 +31,40 @@ export default {
   border-radius: 25px;
   height: 460px;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  text-align: center;
+}
+
+.top-section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.avatar-img {
+  height: 68px;
+  width: 68px;
+  border: 3px solid white;
+  /*border-radius: 50%;*/
+  overflow: hidden;
+}
+
+.avatar-img img {
+  /*border-radius: 50%;*/
+  height: 100%;
+  width: 100%;
+}
+
+.title {
+  font-weight: bold;
+  line-height: 1rem;
+}
+
+.sub-title {
+  font-weight: lighter;
+  font-size: 12px;
 }
 </style>
