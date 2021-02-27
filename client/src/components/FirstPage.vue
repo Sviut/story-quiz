24f9fcbdd9e51e0f17dcc23018d6516b97b25719
Sviut <jeleznov88@gmail.com>
2021-02-27 09:47:40 +0300

    update first page

--- client/src/components/FirstPage.vue
+++ client/src/components/FirstPage.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="wrapper">
-    <div class="image-container">
-      <img src="../assets/images/lazer.jpg" alt="">
-    </div>
-    <div class="text-container">
-      <div class="text-title">
-        Ответьте на несколько простых вопросов и мы запишем вас на процедуру за 190 руб.
+    <div class="card-wrapper">
+      <div class="image-container">
+        <img src="../assets/images/lazer.jpg" alt="">
       </div>
-      <div class="text-subtitle">
-        Опрос займет всего 30 секунд!
-      </div>
+      <div class="text-container">
+        <div class="text-title">
+          Ответьте на несколько простых вопросов и мы запишем вас на процедуру за 190 руб.
+        </div>
+        <div class="text-subtitle">
+          Опрос займет всего 30 секунд!
+        </div>
 
-      <button
-          :disabled="disabled"
-          @click="clickHandler"
-          :style="{background: getGradient}" class="btn"
-      >
-        <i class="far fa-check-circle"></i>
-        Пройти опрос!
-      </button>
+        <button
+            :disabled="disabled"
+            @click="clickHandler"
+            :style="{background: getGradient}" class="btn"
+        >
+          <i class="far fa-check-circle"></i>
+          Пройти опрос!
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -47,25 +49,34 @@ export default {
 </script>
 
 <style scoped>
+
 .wrapper {
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  padding: 20px;
   height: 100%;
+}
+
+.card-wrapper {
+  border-radius: 25px;
+  background-color: white;
 }
 
 .image-container {
-  height: 40%;
   margin-bottom: 40px;
+  border-radius: 20px;
 }
 
 .image-container img {
-  height: 100%;
-  width: 100%;
+  border-top-left-radius: 25px;
+  border-top-right-radius: 25px;
+  max-width: 100%;
   object-fit: cover;
 }
 
 .text-container {
-
+  padding-bottom: 20px;
 }
 
 .text-title {
