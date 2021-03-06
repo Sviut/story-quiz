a8789a1e55207d225dc006463c69b1d48d391388
Sviut <jeleznov88@gmail.com>
2021-03-06 18:31:30 +0300

    fix app styles

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,38 +11,17 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap');
-
-:root {
-  --var-bg-color: rgb(249, 163, 148);
-}
-
 * {
   box-sizing: border-box;
 }
 
 body {
-  overflow: hidden;
-  margin: 0;
-  padding: 0;
+  height: 100vh;
   -webkit-touch-callout: none; /* iOS Safari */
   -webkit-user-select: none; /* Safari */
   -moz-user-select: none; /* Old versions of Firefox */
   -ms-user-select: none; /* Internet Explorer/Edge */
   user-select: none;
   background: linear-gradient(to left top, rgb(132, 133, 216), rgb(233, 235, 250)) no-repeat;
-}
-
-#app {
-  font-family: 'Poppins', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 0;
-  padding: 0;
-  /*height: calc(100vh - 40px);*/
-  height: 100vh;
-  width: 100vw;
 }
 </style>
