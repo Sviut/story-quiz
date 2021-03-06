c5bd44e928d6504bde70bff1e3e2ea0fd5f8e43e
Sviut <jeleznov88@gmail.com>
2021-03-06 13:30:43 +0300

    clear inports

--- client/src/App.vue
+++ client/src/App.vue
@@ -38,7 +38,7 @@
 <script>
 import LongPress from 'vue-directive-long-press'
 
-import QuestionCard from '@/components/QuestionCard'
+import QuestionCard from '@/components/QuestionCard/QuestionCard'
 import ProgressBar from '@/components/ProgressBar'
 import ContactCard from '@/components/ContactCard'
 import FirstPage from '@/components/FirstPage'
@@ -84,7 +84,7 @@ export default {
     nextQuiz () {
       setTimeout(() => {
         this.currentQuiz = this.currentQuiz + 1
-      }, 1300)
+      }, 800)
     },
     submitResults (contact) {
       this.nextQuiz()
@@ -110,49 +110,17 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap');
-
-:root {
-  --var-bg-color: rgb(249, 163, 148);
-}
-
 * {
   box-sizing: border-box;
 }
 
 body {
-  overflow: hidden;
-
   height: 100vh;
-  /*margin: 0;*/
-  /*padding: 0;*/
   -webkit-touch-callout: none; /* iOS Safari */
   -webkit-user-select: none; /* Safari */
   -moz-user-select: none; /* Old versions of Firefox */
   -ms-user-select: none; /* Internet Explorer/Edge */
   user-select: none;
   background: linear-gradient(to left top, rgb(132, 133, 216), rgb(233, 235, 250)) no-repeat;
-}
-
-#app {
-  /*font-family: 'Poppins', sans-serif;*/
-  /*-webkit-font-smoothing: antialiased;*/
-  /*-moz-osx-font-smoothing: grayscale;*/
-  /*text-align: center;*/
-  /*color: #2c3e50;*/
-  /*margin: 0;*/
-  /*padding: 0;*/
-  /*!*height: calc(100vh - 40px);*!*/
-  /*height: 100vh;*/
-  /*width: 100vw;*/
-}
-
-.main {
-  position: relative;
-  /*height: 100%;*/
-}
-
-.container {
-  padding: 20px;
 }
 </style>
---
+++ client/src/components/QuestionCard/QuestionCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="bg-white rounded-3xl">
+    <div
+        class="bg-gradient-to-r from-purple-400 via-pink-500 to-red-400 text-white p-5 uppercase leading-4 rounded-t-3xl text-center text-sm md:text-2xl"
+    >
+      {{ quiz.title }}
+    </div>
+    <div class="flex flex-col space-y-2 p-2">
+      <AnswerButton
+          @click.native="selectAnswer(answer)"
+          v-for="(answer, idx) in quiz.answers" :key="idx"
+          :text="answer.text"
+          :index="idx"
+          :is-selected="selectedAnswer === answer"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import AnswerButton from '@/components/QuestionCard/AnswerButton'
+
+export default {
+  name: 'QuestionCard',
+  components: { AnswerButton },
+  props: {
+    quiz: {
+      type: Object,
+      required: true,
+    },
+    color: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: function () {
+    return {
+      selectedAnswer: '',
+      disabled: false,
+    }
+  },
+  methods: {
+    selectAnswer (answer) {
+      if (this.disabled) {
+        return
+      }
+      this.disabled = true
+      this.selectedAnswer = answer
+      this.$emit('addNewAnswer', { title: this.quiz.title, answer: this.selectedAnswer.text })
+      setTimeout(() => this.disabled = false, 1300)
+    },
+  },
+  computed: {
+    getGradient () {
+      return `linear-gradient(to right, rgb(${this.color.from}), rgb(${this.color.to}))`
+    },
+  },
+}
+</script>
